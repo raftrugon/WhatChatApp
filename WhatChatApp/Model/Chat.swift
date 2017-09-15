2d6babc486a64e797cfdf4273ecb0415830e3d7c
rafael trujillo Gonzalez <raftrugon@alum.us.es>
2017-09-15 12:43:20 +0200

    Creación del estilo de las celdas y del modelo de datos "Chat"

---
+++ WhatChatApp/Model/Chat.swift
@@ -0,0 +1,20 @@
+//
+//  Chat.swift
+//  WhatChatApp
+//
+//  Created by rafa on 15/9/17.
+//  Copyright © 2017 rafa trujillo. All rights reserved.
+//
+
+import UIKit
+
+class Chat {
+    var userName:String?
+    var text:String?
+    var datePost:String?
+    init(userName:String,text:String,datePost:String) {
+        self.userName =  userName
+        self.text =  text
+        self.datePost = datePost
+    }
+}
