ba99a3503f281485f62c672d7fd00604abc7843c
rafael trujillo Gonzalez <raftrugon@alum.us.es>
2017-09-15 12:50:11 +0200

    métodos del delegado tableView

--- WhatChatApp/Controllers/mainVC.swift
+++ WhatChatApp/Controllers/mainVC.swift
@@ -10,13 +10,15 @@ import UIKit
 import FirebaseAuth
 import FirebaseDatabase
 
-class mainVC: UIViewController {
+class mainVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
     var ref = DatabaseReference.init()
     var nameUser:String?
+    var chatInfo = [Chat]()
     
     //IBOutlets
     @IBOutlet var textFieldName: UITextField!
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +26,24 @@ class mainVC: UIViewController {
         loginAnony()
       
         self.ref = Database.database().reference()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
  
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatInfo.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellChat:ChatRoomCell = tableView.dequeueReusableCell(withIdentifier: "chatRoomCell", for: indexPath) as! ChatRoomCell
+        cellChat.setChat(chat: chatInfo[indexPath.row])
+        return cellChat
+    }
+    
 
     
     func loginAnony(){
